1b52f9a2ddcbed5bfa7167a71378add4fd7a8307
ARTHUR DA SILVA HAMES <arthur_hames@estudante.sesisenai.org.br>
2025-03-13 08:47:12 -0300

    cnpj certo

--- cnpj.js
+++ cnpj.js
@@ -7,19 +7,32 @@ faço a divisão
 pego o modulo da divisão
 verificao se o resultado é menor q dois
 */
-let cnpj = Math.floor(Math.random() * 100000000)
-cnpj = cnpj.toString() + '0001'
-array_cnpj = cnpj.split('')
-array_multiplicaçao = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
-let soma =0
-for(let i=0; i<array_cnpj.length; i++){
-    soma += array_cnpj[i] *array_multiplicaçao[i]
+
+function multiplicarNumero(array_cnpj, array_multiplicaçao){
+    let soma =0
+    for(let i=0; i<array_cnpj.length; i++){
+        soma += array_cnpj[i] *array_multiplicaçao[i]
+    }
+    let resto = soma %11
+    let digito
+    if(resto<2){
+        digito =0
+    }else{
+        digito= 11-resto
+    }
+    return digito
 }
-let resto = soma %11
-let digito1
-if(resto<2){
-    digito1 =0
-}else{
-    digito1= 11-resto
+function gerarCNPJ(){
+    let numero_base = Math.floor(Math.random() * 100000000)
+    numero_base = numero_base.toString() + '0001'
+    array_cnpj = numero_base.split('')
+    array_multiplicaçao = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    let digito1 = multiplicarNumero(array_cnpj, array_multiplicaçao) // retorna digito1
+    array_cnpj.push(digito1.toString()) // pegar digito1 acrescentar no array_cnpj
+    array_multiplicaçao.unshift(6) //acrescentar um valor no array_multiplicação
+    let digito2 = multiplicarNumero(array_cnpj, array_multiplicaçao) //retorna digito2
+    array_cnpj.push(digito2.toString())
+    return array_cnpj.join('')
 }
-console.log(digito1)
+let cnpj = gerarCNPJ()
+console.log(cnpj)
