4530d0ffa1cd6e894af698e0b4044eff4642319d
ARTHUR DA SILVA HAMES <arthur_hames@estudante.sesisenai.org.br>
2025-03-13 11:12:43 -0300

    commit creditCD

---
+++ creditCD.js
@@ -0,0 +1,22 @@
+/*
+VERIFICAR
+
+1º dígito Identifica a bandeira do cartão, como Visa (4), Mastercard (51 a 55), 
+American Express (34 a 37), Elo (5 ou 6), e Discover (6)
+2º a 6º dígitos Identificam a instituição financeira emissora do cartão 
+7º a 15º dígitos Identificam a conta do titular do cartão 
+Último dígito O dígito verificador, que indica se a sequência numérica é válida 
+CVV Um código de segurança de 3 dígitos que fica no verso do cartão 
+Data de validade Composta por 4 dígitos, que indicam o mês e ano em que o cartão vence 
+*/
+function bandeira(){
+    for(){
+        
+    }
+
+}
+
+
+const array_numero = [5, 3, 7, 1, 1, 0, 0, 0, 9, 4, 5, 0, 2, 0, 4, 2]
+const array_validade = [1, 2, 3, 0]
+const array_cvv = [3, 4, 2]
